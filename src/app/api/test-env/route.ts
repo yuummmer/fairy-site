646c8e59a9326d64edac3c2dd36a9c85251880db
Jennifer Slotnick <jennifer.slotnick@gmail.com>
2025-10-20 17:47:51 -0700

    Remove test endpoint - Google Sheets integration working

--- src/app/api/test-env/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  return Response.json({
-    hasGoogleScriptUrl: !!process.env.GOOGLE_SCRIPT_URL,
-    urlLength: process.env.GOOGLE_SCRIPT_URL?.length || 0
-  });
-}
